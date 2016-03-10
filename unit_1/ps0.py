d1dfc63fac1f5a4cc1ecb31b07146e3a2f832a72
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-03-10 13:06:45 +0700

    Problem Set 0 - Submitted

---
+++ unit_1/ps0.py
@@ -0,0 +1,10 @@
+# Problem Set 0
+# Name: Mochamad Iqbal Dwi Cahyo
+# Collaborators: John Doe
+# Time Spent: 5:00 
+
+birthdate = raw_input("Enter your date of birth: \n")
+lastname = raw_input("Enter your last name: \n")
+show = lastname + " " + birthdate
+
+print show
