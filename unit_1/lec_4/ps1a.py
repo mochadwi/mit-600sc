d85f0a84e0f97726d7584e46e07b28fa380bfaaf
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-03-16 22:50:47 +0700

    Finish Problem Set 1, and add a solution
    Add doc string, and remove unecesary variable

--- unit_1/lec_4/ps1a.py
+++ unit_1/lec_4/ps1a.py
@@ -1,34 +1,25 @@
-# Declare a first initialization of variable
-# min_monthly_payment = 0
-# interest_paid = 0
-# principal_paid = 0
-# remaining_balance = 0
-# total_amount_paid = 0
-
-def paying_the_minimum(bal, ann_ir, mon_pr):
-	# Calculate the credit card
-	min_monthly_payment = mon_pr * bal
-	interest_paid = ann_ir / 12 * bal
-	principal_paid = min_monthly_payment - interest_paid
-	remaining_balance = bal - principal_paid
-	total_amount_paid = total_amount_paid + min_monthly_payment
-
-	# For iterates each month
-	for month in range(1, 13):
-		# Display the result
-		print "Month: " + str(month)
-		print "Minimum monthly payment: $" + str(min_monthly_payment)
-		print "Principle paid: $" + str(principal_paid)
-		print "Remaining balance: $" + str(remaining_balance)
-
-	print "RESULT"
-	print "Total amount paid: $" + str(total_amount_paid)
-	print "Remaining balance: $" + str(remaining_balance)
-
 # Receive user input
-balance = raw_input("Your balance: ")
-annual_interest_rate = raw_input("Your annual interest rate: ")
-monthly_payment_rate = raw_input("Your monthly payment rate: ")
+balance = float(raw_input("Your balance: "))
+annual_interest_rate = float(raw_input("Your annual interest rate: "))
+monthly_payment_rate = float(raw_input("Your monthly payment rate: "))
 
-# Run the function
-paying_the_minimum(float(balance), float(annual_interest_rate), float(monthly_payment_rate));
+# global variable, to save state
+monthly_interest_rate = annual_interest_rate/12
+total_amount_paid = 0
+
+for month in range(1, 13):
+	# Calculate the credit card
+	min_monthly_payment = round(monthly_payment_rate * balance, 2)
+	interest_paid = round(monthly_interest_rate * balance, 2)
+	principal_paid = min_monthly_payment - interest_paid
+	balance -= principal_paid
+	total_amount_paid += min_monthly_payment
+
+	# Display the result
+	print "Month: ", month
+	print "Minimum monthly payment: $", min_monthly_payment
+	print "Remaining balance: $", balance
+
+print "RESULT"
+print "Total amount paid: $", total_amount_paid
+print "Remaining balance: $", balance
