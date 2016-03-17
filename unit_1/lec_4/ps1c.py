ba6d8b6d77c191e25f53f35049811921f97ae1dd
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-03-17 15:59:08 +0700

    [FIX] Finish Problem Set 1: C, adding a solution, using round() function
    [FIX] wrong display, using round() function

--- unit_1/lec_4/ps1c.py
+++ unit_1/lec_4/ps1c.py
@@ -6,3 +6,38 @@ balance = initialBalance
 monthlyInterestRate = interestRate / 12
 lowerBoundPay = balance / 12
 upperBoundPay = (balance * (1 + monthlyInterestRate) ** 12) / 12
+
+while True:
+    balance = initialBalance
+    monthlyPayment = (lowerBoundPay + upperBoundPay) / 2 # bisection search
+
+    for month in range(1,13):
+        interest = round(balance * monthlyInterestRate, 2)
+        balance += interest - monthlyPayment
+        if balance <= 0:
+            break
+
+    if (upperBoundPay - lowerBoundPay < 0.005):
+        # Print result
+        print "RESULT"
+
+        monthlyPayment = round(monthlyPayment + 0.004999, 2)
+        print "Monthly Payment to pay (1 Year): $", round(monthlyPayment, 2)
+
+        # recalculate
+        balance = initialBalance
+        for month in range(1,13):
+            interest = round(balance * monthlyInterestRate, 2)
+            balance += interest - monthlyPayment
+            if balance <= 0:
+                break
+
+        print "Months needed: ", month
+        print "Your balance: $", round(balance, 2)
+        break
+    elif balance < 0:
+        # Paying too much
+        upperBoundPay = monthlyPayment
+    else:
+        # Paying too little
+        lowerBoundPay = monthlyPayment
